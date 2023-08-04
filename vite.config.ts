346b69013b766b654a0a837333bd6f91b4ca8b2f
Facundo Tenuta <facu_201117@hotmail.com>
2023-08-03 22:55:31 -0300

    🔧 Project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true,
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			'@src': '/src',
+			'@components': '/src/components',
+			'@pages': '/src/pages',
+		},
+	},
+	plugins: [react()],
+});
